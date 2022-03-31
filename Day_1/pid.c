03e9532a403f88ec7aa9a47b2bacedfbde118eea
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-30 17:16:51 -0700

    fix: betty style

--- Day_1/pid.c
+++ Day_1/pid.c
@@ -2,22 +2,25 @@
 #include <unistd.h>
 
 /**
- * main - PID
- *
+ * main - finds and prints currant and parent PIDs
+ * Description - my_pid is the pid for current process;
+ * p_pid is the pid for parent process
+ * test is testing that a pid is stored in an unsigned int
  * Return: Always 0.
  */
 int main(void)
 {
-    pid_t my_pid;
-    pid_t p_pid;
-    unsigned int test;
+	pid_t my_pid;
+	pid_t p_pid;
+	unsigned int test;
 
-    test = getpid();
-    my_pid = getpid();
-    p_pid = getppid();
+	test = getpid();
+	my_pid = getpid();
+	p_pid = getppid();
 
-    printf("pid: %u\n", my_pid);
-    printf("test pid: %u\n", test);
-    printf("ppid: %u\n", p_pid);
-    return (0);
+	printf("pid: %u\n", my_pid);
+	printf("test pid: %u\n", test);
+	printf("ppid: %u\n", p_pid);
+
+	return (0);
 }
